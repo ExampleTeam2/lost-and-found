deb89319f39a0d1bec627cd4a3a798c38465d134
Linus Schlumberger <l@mll.one>
2024-02-23 14:10:16 +0100

    feat: add hslu scraping example

--- scraping/example.spec.ts
+++ scraping/example.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
+import fs from 'fs';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const DATA_PATH = 'scraping/data/';
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// Go to "hslu.ch", navigate to "Informatik", print the text, look for a field of study containing "Artificial Intelligence" and take a screenshot of that field of study.
+test('hslu.ch', async ({ page }) => {
+  await page.goto('https://www.hslu.ch');
+  await page.click('text=Informatik');
+  const text = await page.textContent('body');
+  // Format into json of sub sections and write into a file async (log if error)
+  fs.writeFile(DATA_PATH + 'hslu.json', JSON.stringify(text), (err) => {
+    if (err) console.log(err);
+  });
+  const ai = await page.$('text=Artificial Intelligence');
+  expect(ai).toBeTruthy();
+  await ai?.screenshot({ path: DATA_PATH + 'ai.png' });
 });
