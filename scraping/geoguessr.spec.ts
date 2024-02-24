d5c6ca52ac82728a2ae870ae16f79ecb4fdb8cbc
Linus Schlumberger <l@mll.one>
2024-02-24 20:58:23 +0100

    feat: increase limits and add logs

--- scraping/geoguessr.spec.ts
+++ scraping/geoguessr.spec.ts
@@ -8,9 +8,9 @@ const LOCATION_FILE = 'geoguessr_location_';
 const LOCATION_FILE_EXTENSION = '.png';
 const RESULT_FILE = 'geoguessr_result_';
 const RESULT_FILE_EXTENSION = '.json';
-const MAX_ROUNDS = 10;
-const MAX_GAMES = 5;
-const MAX_MINUTES = 10;
+const MAX_ROUNDS = 15;
+const MAX_GAMES = 100;
+const MAX_MINUTES = 1440;
 
 const getButtonWithText = (page: Page, text: string) => {
   return page.locator('button, a').getByText(text);
@@ -60,13 +60,16 @@ const removeCookieBanner = async (page: Page) => {
 }
 
 const logIn = async (page: Page) => {
+  console.log('Log in needed');
   // Fail the test if the environment variables are not set
   expect(process.env.GEOGUESSR_EMAIL).toBeTruthy();
   expect(process.env.GEOGUESSR_PASSWORD).toBeTruthy();
   // Will never reach this point if the environment variables are not set, but for type checking.
   if (process.env.GEOGUESSR_EMAIL === undefined || process.env.GEOGUESSR_PASSWORD === undefined) return;
+  console.log('Navigating to login page');
   await page.getByText('Log in').click();
   await page.waitForTimeout(1000);
+  console.log('Entering credentials');
   // Wait for page change
   const email = page.locator('input[name="email"]');
   await email.waitFor({ state: 'visible' });
@@ -75,9 +78,11 @@ const logIn = async (page: Page) => {
   await password.waitFor({ state: 'visible' });
   await password.fill(process.env.GEOGUESSR_PASSWORD);
   await page.waitForTimeout(1000);
+  console.log('Attempting log in');
   await clickButtonWithText(page, 'Log in', -1);
   // Wait for logged in page to load
   await getButtonWithText(page, 'Multiplayer').waitFor({ state: 'visible' });
+  console.log('Logged in successfully');
   // If logged in, save the cookies
   const cookies = await page.context().cookies();
   fs.writeFile(DATA_PATH + 'cookies.json', JSON.stringify(cookies), (err) => {
@@ -112,6 +117,7 @@ ${viewerSelector} {
 
 const round = async(page: Page) => {
   const roundId = randomUUID();
+  console.log('Starting round - ' + roundId);
   await page.waitForTimeout(1000);
   // Wait for the street view to load
   const viewer = page.locator('.mapsConsumerUiSceneCoreScene__canvas').first();
@@ -130,12 +136,14 @@ const round = async(page: Page) => {
   const resultText = await result.textContent();
   // The sentence is like "[.]?[...] right answer was [in | indeed | actually | ...] [country].[...][.]?", parse the country.
   const country = resultText?.split('right answer was')[1].split('.')[0].trim();
+  console.log('It was ' + country);
   fs.writeFile(DATA_PATH + RESULT_FILE + roundId + RESULT_FILE_EXTENSION, JSON.stringify(country), (err) => {
     if (err) console.log(err);
   });
 };
 
 const game = async (page: Page) => {
+  console.log('Starting game');
   let rounds = 1;
   await round(page);
   await page.waitForTimeout(1000);
@@ -166,6 +174,8 @@ test('play country battle royale', async ({ page }) => {
   if (!(await checkIfLoggedIn(page))) {
     await removeCookieBanner(page);
     await logIn(page);
+  } else {
+    console.log('Already logged in');
   }
   page.setDefaultTimeout(5000);
   await clickButtonWithText(page, 'Multiplayer', -1);
