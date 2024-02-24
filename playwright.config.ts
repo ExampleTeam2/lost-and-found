72622ac0fb24fd09ed4e1817013d103dbbb9b1b7
Linus Schlumberger <l@mll.one>
2024-02-24 19:49:37 +0100

    fix: disable video to save on memory

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,8 +33,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    screenshot: 'only-on-failure'
   },
 
   /* Configure projects for major browsers */
