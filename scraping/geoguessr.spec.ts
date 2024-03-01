900cf6a8e0883b325421e0ab0da0993a19c1105b
Linus Schlumberger <l@mll.one>
2024-03-01 10:58:33 +0100

    fix: debug count

--- scraping/geoguessr.spec.ts
+++ scraping/geoguessr.spec.ts
@@ -236,6 +236,7 @@ const game = async (page: Page, i: number, identifier?: string) => {
   await page.getByText('Game starting in').or(page.getByText('Rate limit')).nth(0).waitFor({ state: 'visible', timeout: 60000 });
   if (await page.getByText('Rate limit').count() > 0) {
     log('Rate-limited', identifier);
+    fs.writeFileSync(TEMP_PATH + 'last-rate-limits', i + '\n');
     await page.waitForTimeout(STAGGER_INSTANCES);
     await play(page, i, identifier);
     return;
@@ -244,6 +245,7 @@ const game = async (page: Page, i: number, identifier?: string) => {
   const gameId = page.url().split('/').pop() ?? 'no_id_' + randomUUID();
   if (fs.readFileSync(TEMP_PATH + 'games', 'utf8')?.split(/\n/g)?.includes(gameId)) {
     log('Double-joined game', identifier);
+    fs.writeFileSync(TEMP_PATH + 'last-double-joins', i + '\n');
     await page.waitForTimeout(STAGGER_INSTANCES);
     await play(page, i, identifier);
     return;
@@ -318,6 +320,11 @@ describe('Geoguessr', () => {
     }
     fs.appendFileSync(TEMP_PATH + 'games', '');
     fs.writeFileSync(TEMP_PATH + 'initial', 'true');
+    fs.writeFileSync(TEMP_PATH + 'last-rate-limits', '');
+    fs.writeFileSync(TEMP_PATH + 'last-double-joins', '');
+    // Generate a timestamp string
+    const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+    fs.writeFileSync(TEMP_PATH + 'last-start', timestamp);
   });
   for (let i = 0; i < NUMBER_OF_INSTANCES; i++) {
     const identifier = (NUMBER_OF_INSTANCES > 1 ? String(i + 1) : '');
