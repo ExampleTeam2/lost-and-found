62fc04f23e2ae205ba1711ca0b278e198e4f812f
Linus Schlumberger <l@mll.one>
2024-03-01 10:08:31 +0100

    fix: fix reports config

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 5 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: !process.env.CI ? [['json', { outputFile: `./scraping/reports/results_${timestamp}.json` }]] : 'html',
+  reporter: process.env.CI ? [['json', { outputFile: `./scraping/reports/results_${timestamp}.json` }]] : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
