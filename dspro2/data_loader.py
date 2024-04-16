bf8781abeaf4fa742d42a4ce6d1ae9e7a7821599
Linus Schlumberger <l@mll.one>
2024-04-16 22:56:06 +0200

    feat: allow additional regions and fix countries mapping

--- dspro2/data_loader.py
+++ dspro2/data_loader.py
@@ -14,7 +14,7 @@ def _get_counterpart(file):
   elif file.endswith('.png'):
     counterpart = file.replace('location', 'result').replace('.png', '.json')
   else:
-    raise ValueError("Invalid file type")
+    raise ValueError('Invalid file type')
   return counterpart
     
 # Get rid of unpaired files (where only either json or png is present)
@@ -59,9 +59,20 @@ def _get_files_counterparts(files, all_files):
     counterpart = _get_counterpart(file)
     if counterpart in all_files:
       files_counterparts.append(counterpart)
+      files_counterparts.append(file)
     else:
       files_counterparts.append(file)
   return files_counterparts
+
+# Replace one start of the file with another and create a map from the original files to the new ones, then return the new files and the map
+def _convert_to_fake_locations(files, start, new_start):
+  fake_files = [file.replace(start, new_start) for file in files]
+  fake_locations_map = {file: new_file for file, new_file in zip(files, fake_files)}
+  return fake_files, fake_locations_map
+
+# From a list of files and a map, restore the original locations
+def _restore_from_fake_locations(files, fake_locations_map):
+  return [fake_locations_map.get(file, file) for file in files]
 
 # Takes in a list of files and a occurrence map (from a different_dataset)), create an optimally mapped list of files where the occurrences correspond to the map (or are multiples of them)
 def map_occurrences_to_files(files, occurrence_map, allow_missing=False):
@@ -76,9 +87,9 @@ def map_occurrences_to_files(files, occurrence_map, allow_missing=False):
       # filter out the missing countries
       factors = [x for x in factors if x == x]
     else:
-      raise ValueError("Missing country in one of the maps")
+      raise ValueError('Missing country in one of the maps')
   if allow_missing and len(factors) == 0:
-    raise ValueError("No countries in commmon between the maps")
+    raise ValueError('No countries in commmon between the maps')
   # Get the lowest factor
   factor = min(factors)
   # Get the number of files to load by country
@@ -93,16 +104,34 @@ def map_occurrences_to_files(files, occurrence_map, allow_missing=False):
   files_counterparts = _get_files_counterparts(files_to_load, [*files, *countries_to_files.values()])
   return files_counterparts, len(files_to_load)
 
-def get_data_to_load(loading_file = './data_list', file_location = os.path.join(os.path.dirname(__file__), '1_data_collection/data'), filterText="singleplayer", type="", limit=0, countries_map=None, allow_missing_in_map=False):
-  all_files = list([file.path for file in os.scandir(file_location)])
+def get_data_to_load(loading_file = './data_list', file_location = os.path.join(os.path.dirname(__file__), '1_data_collection/data'), json_file_location = None, image_file_location = None, filterText='singleplayer', type='', limit=0, countries_map=None, allow_missing_in_map=False, passthrough_map=False):
+  all_locations = []
+  if file_location is not None:
+    all_locations.append([file_location, filterText, type])
+  if json_file_location is not None and type != 'png':
+    all_locations.append([json_file_location, filterText, 'json'])
+  if image_file_location is not None and type != 'json':
+    all_locations.append([image_file_location, filterText, 'png'])
+  all_files = []
+  fake_locations_map = {}
+  for location, current_filter, current_type in all_locations:
+    current_files = list([file.path for file in os.scandir(location)])
+    if file_location != location and type == '' and file_location is not None:
+      current_files, current_fake_locations_map = _convert_to_fake_locations(current_files, location, file_location)
+      # extend fake_locations_maps with current_fake_locations_map
+      fake_locations_map.update(current_fake_locations_map)
+    if filterText:
+      current_files = list(filter(lambda x: current_filter in x and x.endswith(current_type), current_files))
+    all_files.extend(current_files)
   if not type:
     all_files = _remove_unpaired_files(all_files)
-  if filterText:
-    all_files = list(filter(lambda x: filterText in x and x.endswith(type), all_files))
-  if countries_map:
+  if countries_map and not passthrough_map:
     all_files, _ = map_occurrences_to_files(all_files, countries_map, allow_missing=allow_missing_in_map)
   if limit:
     all_files = all_files[:limit]
+    
+  all_files = _restore_from_fake_locations(all_files, fake_locations_map)
+    
   base_files = list([os.path.basename(file) for file in all_files])
   files_to_load = base_files
   
@@ -114,21 +143,21 @@ def get_data_to_load(loading_file = './data_list', file_location = os.path.join(
     pass
       
   if not len(files_to_load):
-    raise ValueError("No files to load, did you forget to specify the type? Otherwise unpaired files will be ignored")
+    raise ValueError('No files to load, did you forget to specify the type? Otherwise unpaired files will be ignored')
   
   if not len(all_files):
-    raise ValueError("No files in loading location")
+    raise ValueError('No files in loading location')
   
   actual_file_locations = []
   
   for file in files_to_load:
     if file not in base_files:
-      raise ValueError("Missing file " + file)
+      raise ValueError('Missing file ' + file)
     else:
       actual_file_locations.append(all_files[base_files.index(file)])
       
   with open(loading_file, 'w') as file:
-    file.write("\n".join(files_to_load))
+    file.write('\n'.join(files_to_load))
     
   return actual_file_locations
 
@@ -144,8 +173,8 @@ def load_json_files(files, num_workers=4):
   return results   
 
 # get countries occurrences from multiplayer games
-def get_countries_occurrences(loading_file = './countries_map_data_list', file_location = os.path.join(os.path.dirname(__file__), '1_data_collection/data')):
-  multiplayer = get_data_to_load(loading_file=loading_file, file_location=file_location, filterText='multiplayer', type='json')
+def get_countries_occurrences(loading_file = './countries_map_data_list', file_location = os.path.join(os.path.dirname(__file__), '1_data_collection/data'), filterText='multiplayer'):
+  multiplayer = get_data_to_load(loading_file=loading_file, file_location=file_location, filterText=filterText, type='json')
   # map data
   countries, countries_to_files, files_to_countries, num_games = get_countries_occurrences_from_files(multiplayer)
   return countries, countries_to_files, files_to_countries, num_games
