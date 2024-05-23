9bf1c404baf6efbd87acf7bf5b4d6ac0e7b66f41
Linus Schlumberger <l@mll.one>
2024-05-23 17:05:14 +0200

    feat: add setup script

---
+++ setup_environ/setup_environ_and_train.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+pip3 install poetry
+git clone https://oauth2:{os.environ["ACCESS_TOKEN"]}@gitlab.com/exampleteam2/dspro2.git
+poetry install
+
+# Define the path to the notebook
+NOTEBOOK_PATH="dspro2/4_modeling/training_resnet_models.ipynb"
+OUTPUT_PATH="temp_executed_notebook.ipynb"
+
+# Activate the poetry environment and execute the notebook
+poetry run jupyter nbconvert --ExecutePreprocessor.timeout=600 --to notebook --execute "$NOTEBOOK_PATH" --output "$OUTPUT_PATH"
