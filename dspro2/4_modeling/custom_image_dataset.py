c4bd87b285a7322801e753c9b7451e1cba5f3e66
Valairaa <75242818+Valairaa@users.noreply.github.com>
2024-05-23 16:03:35 +0200

    new bugfixes with relative paths

--- dspro2/4_modeling/custom_image_dataset.py
+++ dspro2/4_modeling/custom_image_dataset.py
@@ -8,25 +8,33 @@ class CustomImageDataset(Dataset):
         self.images = images
         self.coordinates = coordinates
         self.countries = countries
-        self.country_index_path= f"models/datasize_{datasize}_country_to_index.json"
+        self.country_index_path = os.path.join("models", f"datasize_{datasize}_country_to_index.json")
         absolute_path = os.path.abspath(self.country_index_path)
 
         print(f"Country index path (relative): {self.country_index_path}")
         print(f"Country index path (absolute): {absolute_path}")
 
         if replace_country_index:
-          # Try to load existing country to index mapping from a JSON file
-          with open(self.country_index_path, 'r') as f:
-              self.country_to_index = json.load(f)
-          # Ensure keys read from JSON (which will be strings) are correctly handled if numeric
-          if all(isinstance(key, str) and key.isdigit() for key in self.country_to_index.keys()):
-              # Convert keys from strings to integers if necessary
-              self.country_to_index = {int(k): v for k, v in self.country_to_index.items()}
+            if os.path.exists(self.country_index_path):
+                # Try to load existing country to index mapping from a JSON file
+                with open(self.country_index_path, 'r') as f:
+                    self.country_to_index = json.load(f)
+                # Ensure keys read from JSON (which will be strings) are correctly handled if numeric
+                if all(isinstance(key, str) and key.isdigit() for key in self.country_to_index.keys()):
+                    # Convert keys from strings to integers if necessary
+                    self.country_to_index = {int(k): v for k, v in self.country_to_index.items()}
+            else:
+                print(f"File not found: {self.country_index_path}. Generating new mapping.")
+                self._generate_and_save_country_index()
         else:
-            unique_countries = sorted(set(countries))
-            self.country_to_index = {country: idx for idx, country in enumerate(unique_countries)}
-            with open(self.country_index_path, 'w') as f:
-                  json.dump(self.country_to_index, f)
+            self._generate_and_save_country_index()
+
+    def _generate_and_save_country_index(self):
+        unique_countries = sorted(set(self.countries))
+        self.country_to_index = {country: idx for idx, country in enumerate(unique_countries)}
+        os.makedirs(os.path.dirname(self.country_index_path), exist_ok=True)
+        with open(self.country_index_path, 'w') as f:
+            json.dump(self.country_to_index, f)
 
     def __len__(self):
         return len(self.images)
