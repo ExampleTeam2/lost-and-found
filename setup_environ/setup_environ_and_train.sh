f6440eb6f6203fe82b02b91607f0a92f22d51d15
Linus Schlumberger <l@mll.one>
2024-05-24 14:22:22 +0200

    fix: set access token

--- setup_environ/setup_environ_and_train.sh
+++ setup_environ/setup_environ_and_train.sh
@@ -2,6 +2,7 @@
 pip3 install poetry
 git clone "https://oauth2:$ACCESS_TOKEN@gitlab.com/exampleteam2/dspro2.git"
 cd dspro2
+git remote set-url --push origin "https://oauth2:$ACCESS_TOKEN@gitlab.com/exampleteam2/dspro2.git"
 git pull
 poetry install
 
