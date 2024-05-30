ad87220e4855026fbbc86ee94512022acfe62689
Yutaro Sigrist <yutarosigrist@gmail.com>
2024-05-30 22:02:57 +0200

    region enricher wip

--- dspro2/3_data_preparation/01_enriching/region_enricher.py
+++ dspro2/3_data_preparation/01_enriching/region_enricher.py
@@ -16,13 +16,17 @@ sys.path.insert(0, '../../')
 from data_loader import resolve_env_variable, get_json_files, load_json_file
 from countryconverter import convert_from_coordinates, convert_from_names
 from countryconverter import convert_coordinates_to_country_names
+from shapely.geometry import box
+from itertools import chain
+import warnings
+from scipy.spatial import cKDTree
 
 
 
 class RegionEnricher:
     def __init__(self):
         self.file_path = '../../data/admin_1_states_provinces.json'
-        self.process()
+        warnings.filterwarnings("ignore", message="Geometry is in a geographic CRS")
 
     def load_geojson(self, file):
         gdf = gpd.read_file(filename=file, driver='GeoJSON')
@@ -63,39 +67,63 @@ class RegionEnricher:
 
 
     def load_enriched_geojson(self):
-        gdf = gpd.read_file('../../data/admin_1_states_provinces.geojson', driver='GeoJSON')
+        gdf = gpd.read_file('../../data/admin_1_states_provinces.geojson', driver='GeoJSON', crs='EPSG:4326')
         return gdf
     
-    # function to get the nearest 5 regions to a point and if the point is in a region, it will return the region names
     def get_region_from_point(self, gdf, point):
+        if not isinstance(point, Point):
+            point = Point(point)
         # get the nearest 5 regions to the point
-        nearest_regions = gdf['name_1'].iloc[gdf.distance(point).argsort()[:5]]
+        nearest_regions = gdf['name_en'].iloc[gdf.distance(point).argsort()[:5]]
         return nearest_regions
-    
+
     def is_point_in_region(self, gdf, point):
+        if not isinstance(point, Point):
+            point = Point(point)
         # check if the point is in a region
-        is_in_region = gdf.contains(point)
+        is_in_region = gdf.contains(point).any()
         return is_in_region
     
-    # function to call the get_region_from_point and is_point_in_region functions for a list of points
+
+    # Find the top 5 nearest regions for each point
+    def find_nearest_regions(self, points, regions, k=5):
+        # Get the centroids of the regions
+        centroids = np.array(list(regions.geometry.centroid.apply(lambda x: (x.x, x.y))))
+        
+        # Create a KDTree for fast nearest-neighbor lookup
+        tree = cKDTree(centroids)
+        
+        # Initialize results
+        results = [
+                  [regions.iloc[idx]['name_en'] for idx in tree.query((point.x, point.y), k=k)[1]]
+                  for point in points
+              ]
+
+        return results
+
     def get_region_from_points(self, gdf, points):
-        regions = []
-        is_in_regions = []
-        for point in points:
-            region = self.get_region_from_point(gdf, point)
-            is_in_region = self.is_point_in_region(gdf, point)
-            is_in_regions.append(is_in_region)
-            regions.append(region)
-        return regions, is_in_regions
-    
+        # Create a spatial index for the GeoDataFrame
+
+        # Convert points to a GeoSeries
+        points_gs = gpd.GeoSeries([Point(point) if not isinstance(point, Point) else point for point in points], crs=gdf.crs)
+
+        # Check if the points are in any of the regions
+        is_in_regions = [gdf.contains(point).any() for point in points_gs]
+        print(is_in_regions)
+
+        nearest_regions = self.find_nearest_regions(points_gs, gdf)
+        print(nearest_regions)
+
+
+        return nearest_regions, is_in_regions
+
     def add_regions_to_json(self, coordinates, file_map, json_files, gdf):
-        # get the region names and if the point is in a region
         regions, is_in_regions = self.get_region_from_points(gdf, coordinates)
         for region, is_in_region, coord in zip(regions, is_in_regions, coordinates):
             image_ids = file_map[str(coord)]
             for image_id in image_ids:
                 json_files[image_id]['region_name'] = region
-                json_files[image_id]['is_in_region'] = is_in_region
+                json_files[image_id]['is_in_region'] = str(is_in_region)
         return json_files
 
 
