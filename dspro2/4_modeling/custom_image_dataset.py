2b5991cf585719ac7f7aa9ae95d8a9338150a072
Yutaro Sigrist <yutarosigrist@gmail.com>
2024-06-07 13:10:18 +0200

    remove region_index

--- dspro2/4_modeling/custom_image_dataset.py
+++ dspro2/4_modeling/custom_image_dataset.py
@@ -27,8 +27,6 @@ class CustomImageDataset(Dataset):
         region = self.regions[idx]
         if country not in self.country_to_index:
             raise ValueError(f"Country '{country}' at index {idx} is not in the country_to_index mapping.")
-        if region not in self.region_to_index:
-            raise ValueError(f"Region '{region}' at index {idx} is not in the region_to_index mapping.")
         country_index = self.country_to_index[country]
         region_index = self.region_to_index[region]
         coordinates = torch.tensor(self.coordinates[idx], dtype=torch.float32)
