c625e1b3c98bc953f41cb03b1c685e4831f88523
Linus Schlumberger <l@mll.one>
2024-06-20 22:45:30 +0200

    chore: clone git lfs on colab

--- setup_environ/setup_environ_and_train.sh
+++ setup_environ/setup_environ_and_train.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 pip3 install poetry --quiet
 # Clone the repository withouth the LFS files for now
-export GIT_LFS_SKIP_SMUDGE=1 && git clone "https://oauth2:$ACCESS_TOKEN@gitlab.com/exampleteam2/dspro2.git"
+git clone "https://oauth2:$ACCESS_TOKEN@gitlab.com/exampleteam2/dspro2.git"
 cd dspro2
 echo "Installing dependencies..."
 poetry install --quiet
