299e11ba73ab792b99e08c1b7b2fdad0ee9d30f7
Linus Schlumberger <l@mll.one>
2024-06-22 13:20:56 +0200

    fix: delete zip file after caching

--- dspro2/data_loader.py
+++ dspro2/data_loader.py
@@ -885,6 +885,8 @@ def get_data_to_load(loading_file = './data_list', file_location = os.path.join(
       additional_save_callback = lambda: _save_to_zips_from_tmp(file_location, json_file_location, image_file_location, current_dir, tmp_dir, only_additional=True, skip_additional=False)
     if downloaded_new_files:
       _save_to_zips_from_tmp(file_location, json_file_location, image_file_location, current_dir, tmp_dir, only_additional=False, skip_additional=True)
+    # delete zip file
+    os.remove(os.path.join(current_dir, 'files.zip'))
       
   # if no loading file, use the just discovered files
   files_to_load = basenames
