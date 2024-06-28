aedc372cc718ff5e8e7f13299331d0291d5a5a0a
Linus Schlumberger <l@mll.one>
2024-06-28 14:52:36 +0200

    fix: push to correct project in wandb

--- dspro2/4_modeling/geo_model_evaluator.py
+++ dspro2/4_modeling/geo_model_evaluator.py
@@ -12,68 +12,61 @@ class GeoModelEvaluator(GeoModelInference):
         self.val_dataloader = val_dataloader
 
     def evaluate(self, model_type, model_path, use_balanced_accuracy=False, second_balanced_on_countries_only=None, accuracy_per_country=False, median_metric=False):
-        with wandb.init(reinit=True) as run:
-            # new line
-            print()
-            print(f"Evaluating for run {run.name} ({run.id})")
-            # new line
-            print()
+        self.prepare(model_type=model_type, model_path=model_path)
 
-            self.prepare(model_type=model_type, model_path=model_path)
+        with torch.no_grad():
+            val_epoch = self.run_epoch(self.val_dataloader, is_train=False, use_balanced_accuracy=use_balanced_accuracy, balanced_on_countries_only=None, accuracy_per_country=accuracy_per_country, median_metric=median_metric)
 
+        log = {}
+
+        val_loss = val_epoch["avg_loss"]
+        log["Best Validation Loss"] = val_loss
+        if self.use_coordinates or self.use_regions:
+            val_metric = val_epoch["avg_metric"]
+            log["Best Validation Distance (km)"] = val_metric
+            if median_metric:
+                val_median_metric = val_epoch["median_metric"]
+                log["Best Validation Median Distance (km)"] = val_median_metric
+        if self.use_regions or (not self.use_coordinates):
+            val_top1_accuracy, val_top3_accuracy, val_top5_accuracy = val_epoch["top1_accuracy"], val_epoch["top3_accuracy"], val_epoch["top5_accuracy"]
+            log["Best Validation Accuracy Top 1"] = val_top1_accuracy
+            log["Best Validation Accuracy Top 3"] = val_top3_accuracy
+            log["Best Validation Accuracy Top 5"] = val_top5_accuracy
+            if use_balanced_accuracy:
+                val_top1_balanced_accuracy = val_epoch["top1_balanced_accuracy"]
+                log["Best Validation Balanced Accuracy Top 1"] = val_top1_balanced_accuracy
+            if accuracy_per_country:
+                val_accuracy_per_country = val_epoch["accuracy_per_country"]
+                log["Best Validation Accuracy Per Country"] = val_accuracy_per_country
+        if self.use_regions:
+            val_top1_correct_country, val_top3_correct_country, val_top5_correct_country = val_epoch["top1_correct_country"], val_epoch["top3_correct_country"], val_epoch["top5_correct_country"]
+            log["Best Validation Accuracy Top 1 Country"] = val_top1_correct_country
+            log["Best Validation Accuracy Top 3 Country"] = val_top3_correct_country
+            log["Best Validation Accuracy Top 5 Country"] = val_top5_correct_country
+            if use_balanced_accuracy:
+                val_top1_balanced_accuracy_country = val_epoch["top1_balanced_accuracy_country"]
+                log["Best Validation Balanced Accuracy Top 1 Country"] = val_top1_balanced_accuracy_country
+
+        if (self.use_regions or (not self.use_coordinates)) and use_balanced_accuracy and second_balanced_on_countries_only is not None:
             with torch.no_grad():
-                val_epoch = self.run_epoch(self.val_dataloader, is_train=False, use_balanced_accuracy=use_balanced_accuracy, balanced_on_countries_only=None, accuracy_per_country=accuracy_per_country, median_metric=median_metric)
+                val_epoch_mapped = self.run_epoch(self.val_dataloader, is_train=False, use_balanced_accuracy=use_balanced_accuracy, balanced_on_countries_only=second_balanced_on_countries_only, accuracy_per_country=False, median_metric=False)
+            if not self.use_regions:
+                val_top1_balanced_accuracy_mapped = val_epoch_mapped["top1_balanced_accuracy"]
+                log["Best Validation Balanced Accuracy Top 1 (Mapped)"] = val_top1_balanced_accuracy_mapped
+            else:
+                val_top1_balanced_accuracy_country_mapped = val_epoch_mapped["top1_balanced_accuracy_country"]
+                log["Best Validation Balanced Accuracy Top 1 Country (Mapped)"] = val_top1_balanced_accuracy_country_mapped
 
-            log = {}
+        # Log metrics to wandb
+        for k, v in log.items():
+            wandb.run.summary[k] = v
 
-            val_loss = val_epoch["avg_loss"]
-            log["Best Validation Loss"] = val_loss
-            if self.use_coordinates or self.use_regions:
-                val_metric = val_epoch["avg_metric"]
-                log["Best Validation Distance (km)"] = val_metric
-                if median_metric:
-                    val_median_metric = val_epoch["median_metric"]
-                    log["Best Validation Median Distance (km)"] = val_median_metric
-            if self.use_regions or (not self.use_coordinates):
-                val_top1_accuracy, val_top3_accuracy, val_top5_accuracy = val_epoch["top1_accuracy"], val_epoch["top3_accuracy"], val_epoch["top5_accuracy"]
-                log["Best Validation Accuracy Top 1"] = val_top1_accuracy
-                log["Best Validation Accuracy Top 3"] = val_top3_accuracy
-                log["Best Validation Accuracy Top 5"] = val_top5_accuracy
-                if use_balanced_accuracy:
-                    val_top1_balanced_accuracy = val_epoch["top1_balanced_accuracy"]
-                    log["Best Validation Balanced Accuracy Top 1"] = val_top1_balanced_accuracy
-                if accuracy_per_country:
-                    val_accuracy_per_country = val_epoch["accuracy_per_country"]
-                    log["Best Validation Accuracy Per Country"] = val_accuracy_per_country
-            if self.use_regions:
-                val_top1_correct_country, val_top3_correct_country, val_top5_correct_country = val_epoch["top1_correct_country"], val_epoch["top3_correct_country"], val_epoch["top5_correct_country"]
-                log["Best Validation Accuracy Top 1 Country"] = val_top1_correct_country
-                log["Best Validation Accuracy Top 3 Country"] = val_top3_correct_country
-                log["Best Validation Accuracy Top 5 Country"] = val_top5_correct_country
-                if use_balanced_accuracy:
-                    val_top1_balanced_accuracy_country = val_epoch["top1_balanced_accuracy_country"]
-                    log["Best Validation Balanced Accuracy Top 1 Country"] = val_top1_balanced_accuracy_country
+        # Clean up
+        del self.model
 
-            if (self.use_regions or (not self.use_coordinates)) and use_balanced_accuracy and second_balanced_on_countries_only is not None:
-                with torch.no_grad():
-                    val_epoch_mapped = self.run_epoch(self.val_dataloader, is_train=False, use_balanced_accuracy=use_balanced_accuracy, balanced_on_countries_only=second_balanced_on_countries_only, accuracy_per_country=False, median_metric=False)
-                if not self.use_regions:
-                    val_top1_balanced_accuracy_mapped = val_epoch_mapped["top1_balanced_accuracy"]
-                    log["Best Validation Balanced Accuracy Top 1 (Mapped)"] = val_top1_balanced_accuracy_mapped
-                else:
-                    val_top1_balanced_accuracy_country_mapped = val_epoch_mapped["top1_balanced_accuracy_country"]
-                    log["Best Validation Balanced Accuracy Top 1 Country (Mapped)"] = val_top1_balanced_accuracy_country_mapped
+        gc.collect()
+        torch.cuda.empty_cache()
 
-            # Log metrics to wandb
-            for k, v in log.items():
-                wandb.run.summary[k] = v
-
-            # Clean up
-            del self.model
-
-            gc.collect()
-            torch.cuda.empty_cache()
-
-            # Print results, floats to 4 decimal places
-            for k, v in log.items():
-                print(f"{k}: {v:.4f}")
+        # Print results, floats to 4 decimal places
+        for k, v in log.items():
+            print(f"{k}: {v:.4f}")
